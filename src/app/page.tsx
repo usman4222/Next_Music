969f039f304b9ad9a861ed97057422f129a47e6c
usman4222 <usman4243ch@gmail.com>
2025-04-04 16:06:06 +0500

    complete WhyChooseUsSection

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import CoursesSections from "@/compnents/CoursesSections";
 import HeroSection from "@/compnents/HeroSection";
+import WhyChooseUsSection from "@/compnents/WhyChooseUsSection";
 
 export default function Home() {
   return (
     <>
       <HeroSection />
       <CoursesSections/>
+      <WhyChooseUsSection/>
     </>
   );
 }
