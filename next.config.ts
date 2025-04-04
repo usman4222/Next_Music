08699eabfe6a4dc7b94f1812a2b4d104435423fc
usman4222 <usman4243ch@gmail.com>
2025-04-05 01:19:27 +0500

    complete instructor section woth Wavy Background

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    domains: ["source.unsplash.com", "images.unsplash.com"],
+  },
 };
 
 export default nextConfig;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import CoursesSections from "@/compnents/CoursesSections";
 import HeroSection from "@/compnents/HeroSection";
 import InfiniteMovingCardsSection from "@/compnents/InfiniteMovingCardsSection";
+import Instructor from "@/compnents/Instructor";
 import WhyChooseUsSection from "@/compnents/WhyChooseUsSection";
 
 export default function Home() {
@@ -10,6 +11,7 @@ export default function Home() {
       <CoursesSections/>
       <WhyChooseUsSection/>
       <InfiniteMovingCardsSection/>
+      <Instructor/>
     </>
   );
 }
