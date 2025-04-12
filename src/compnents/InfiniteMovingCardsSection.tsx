8d46646f3f40d6ed7bb62af8468f8869368bb02d
usman4222 <usman4243ch@gmail.com>
2025-04-12 23:59:44 +0500

    update testimonials

--- src/compnents/InfiniteMovingCardsSection.tsx
+++ src/compnents/InfiniteMovingCardsSection.tsx
@@ -9,7 +9,7 @@ const testimonials = [
     quote:
       "It was the best of times, it was the worst of times, it was the age of wisdom, it was the age of foolishness, it was the epoch of belief, it was the epoch of incredulity, it was the season of Light, it was the season of Darkness, it was the spring of hope, it was the winter of despair.",
     name: "Charles Dickens",
-    title: "A Tale of Two Cities",
+    title: "A Tale of Two ",
   },
   {
     quote:
